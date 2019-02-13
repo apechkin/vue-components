e5e58a85e712c4af1acc4fa75834fc83c5f408d9
Anatoliy Pechkin <anatmozg@mail.ru>
2019-02-13 23:27:42 +0300

    initial commit for vue-components

---
+++ src/components/CostItem/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="item" :style="styles" :draggable="draggable">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    draggable: {
+      type: Boolean,
+      default: false
+    },
+    styles: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .item {
+    display: inline-block;
+    border: 4px salmon solid;
+    height: 160px;
+    width: 160px;
+    margin: 10px;
+    background-color: transparent;
+  }
+</style>
---
+++ src/components/TestPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="header">This is page for test components</div>
+    <div class="body">
+      <cost-item>
+        <div class="fill" draggable="true"></div>
+      </cost-item>
+      <cost-item draggable></cost-item>
+      <cost-item draggable></cost-item>
+      <cost-item draggable :style="{backgroundColor: 'blue', border: '4px green solid'}"></cost-item>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+<script>
+import CostItem from './CostItem'
+export default {
+  components: {
+    CostItem
+  },
+  data () {
+    return {
+
+    }
+  },
+  created () {
+  },
+  beforeDestroy () {
+  }
+}
+</script>
+<style lang="scss" scoped>
+ .fill {
+    background-image: url('http://source.unsplash.com/random/150x150');
+    position: relative;
+    width: 150px;
+    height: 150px;
+    top: 5px;
+    left: 5px;
+    cursor: pointer;
+  }
+</style>
