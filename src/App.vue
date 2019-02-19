ba62189eb996f440a52642a4ec6326b0658a819b
Anatoliy Pechkin <anatmozg@mail.ru>
2019-02-19 16:49:21 +0300

    set backgound color

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,21 @@
   </div>
 </template>
 <script>
-import LHeader from './pages/Layout/Header'
-import LFooter from './pages/Layout/Footer'
-import LContent from './pages/Layout/Content'
-export default {
-  components: {
-    LHeader,
-    LFooter,
-    LContent
+  import LHeader from './pages/Layout/Header'
+  import LFooter from './pages/Layout/Footer'
+  import LContent from './pages/Layout/Content'
+  export default {
+    components: {
+      LHeader,
+      LFooter,
+      LContent
+    }
   }
-}
 </script>
 <style lang="scss">
   html, body {
     height: 100%;
     margin: 0;
+    background-color: #dee2e6;
   }
 </style>
