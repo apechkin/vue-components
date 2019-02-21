661ce9a22b8e7e009f22daf6205f41ea9a34aa59
Anatoliy Pechkin <anatmozg@mail.ru>
2019-02-21 15:45:01 +0300

    add fake data

---
+++ src/pages/Content/fakeData.js
@@ -0,0 +1,309 @@
+export const calendar = () => {
+  return [{
+    year: 2019,
+    months: [
+      {
+        name: 'January',
+        weeks: [
+          {
+            number: 1,
+            days: [{
+              number: 31,
+              fullDate: '31.12.2018'
+            }, {
+              number: 1,
+              fullDate: '01.01.2019'
+            }, {
+              number: 2,
+              fullDate: '02.01.2019'
+            }, {
+              number: 3,
+              fullDate: '03.01.2019'
+            }, {
+              number: 4,
+              fullDate: '04.01.2019'
+            }, {
+              number: 5,
+              fullDate: '05.01.2019'
+            }, {
+              number: 6,
+              fullDate: '06.01.2019'
+            }]
+          },
+          {
+            number: 2,
+            days: [
+              {
+                number: 7,
+                fullDate: '07.01.2019'
+              }, {
+                number: 8,
+                fullDate: '08.01.2019'
+              }, {
+                number: 9,
+                fullDate: '09.01.2019'
+              }, {
+                number: 10,
+                fullDate: '10.01.2019'
+              }, {
+                number: 11,
+                fullDate: '11.01.2019'
+              }, {
+                number: 12,
+                fullDate: '12.01.2019'
+              }, {
+                number: 13,
+                fullDate: '13.01.2019'
+              }]
+          },
+          {
+            number: 3,
+            days: [
+              {
+                number: 14,
+                fullDate: '14.01.2019'
+              }, {
+                number: 15,
+                fullDate: '15.01.2019'
+              }, {
+                number: 16,
+                fullDate: '16.01.2019'
+              }, {
+                number: 17,
+                fullDate: '17.01.2019'
+              }, {
+                number: 18,
+                fullDate: '18.01.2019'
+              }, {
+                number: 19,
+                fullDate: '19.01.2019'
+              }, {
+                number: 20,
+                fullDate: '20.01.2019'
+              }]
+          },
+          {
+            number: 4,
+            days: [
+              {
+                number: 21,
+                fullDate: '21.01.2019'
+              }, {
+                number: 22,
+                fullDate: '22.01.2019'
+              }, {
+                number: 23,
+                fullDate: '23.01.2019'
+              }, {
+                number: 24,
+                fullDate: '24.01.2019'
+              }, {
+                number: 25,
+                fullDate: '25.01.2019'
+              }, {
+                number: 26,
+                fullDate: '26.01.2019'
+              }, {
+                number: 27,
+                fullDate: '27.01.2019'
+              }]
+          },
+          {
+            number: 5,
+            days: [
+              {
+                number: 28,
+                fullDate: '28.01.2019'
+              }, {
+                number: 29,
+                fullDate: '29.01.2019'
+              }, {
+                number: 30,
+                fullDate: '30.01.2019'
+              }, {
+                number: 31,
+                fullDate: '31.01.2019'
+              }, {
+                number: 1,
+                fullDate: '01.02.2019'
+              }, {
+                number: 2,
+                fullDate: '02.02.2019'
+              }, {
+                number: 3,
+                fullDate: '03.02.2019'
+              }]
+          }]
+      },
+      {
+        name: 'February',
+        weeks: [{
+          number: 6,
+          days: [
+            {
+              number: 4,
+              fullDate: '04.02.2019'
+            }, {
+              number: 5,
+              fullDate: '05.02.2019'
+            }, {
+              number: 6,
+              fullDate: '06.02.2019'
+            }, {
+              number: 7,
+              fullDate: '07.02.2019'
+            }, {
+              number: 8,
+              fullDate: '08.02.2019'
+            }, {
+              number: 9,
+              fullDate: '09.02.2019'
+            }, {
+              number: 10,
+              fullDate: '10.02.2019'
+            }]
+        },
+        {
+          number: 7,
+          days: [
+            {
+              number: 11,
+              fullDate: '11.02.2019'
+            }, {
+              number: 12,
+              fullDate: '12.02.2019'
+            }, {
+              number: 13,
+              fullDate: '13.02.2019'
+            }, {
+              number: 14,
+              fullDate: '14.02.2019'
+            }, {
+              number: 15,
+              fullDate: '15.02.2019'
+            }, {
+              number: 16,
+              fullDate: '16.02.2019'
+            }, {
+              number: 17,
+              fullDate: '17.02.2019'
+            }]
+        },
+        {
+          number: 8,
+          days: [
+            {
+              number: 18,
+              fullDate: '18.02.2019'
+            }, {
+              number: 19,
+              fullDate: '19.02.2019'
+            }, {
+              number: 20,
+              fullDate: '20.02.2019'
+            }, {
+              number: 21,
+              fullDate: '21.02.2019'
+            }, {
+              number: 22,
+              fullDate: '22.02.2019'
+            }, {
+              number: 23,
+              fullDate: '23.02.2019'
+            }, {
+              number: 24,
+              fullDate: '24.02.2019'
+            }]
+        },
+        {
+          number: 9,
+          days: [
+            {
+              number: 25,
+              fullDate: '25.02.2019'
+            }, {
+              number: 26,
+              fullDate: '26.02.2019'
+            }, {
+              number: 27,
+              fullDate: '27.02.2019'
+            }, {
+              number: 28,
+              fullDate: '28.02.2019'
+            }, {
+              number: 1,
+              fullDate: '01.03.2019'
+            }, {
+              number: 2,
+              fullDate: '02.03.2019'
+            }, {
+              number: 3,
+              fullDate: '03.03.2019'
+            }]
+        }]
+      }]
+  }]
+}
+
+export const costItems = () => {
+  return [
+    {
+      id: 1514,
+      name: 'Do excepteur est culpa et commodo duis.',
+      values: [{
+        date: '31.12.2018',
+        value: 15000
+      }, {
+        date: '31.01.2019',
+        value: 25000
+      }]
+    },
+    {
+      id: 2256,
+      name: 'Do excepteur est culpa et commodo duis.',
+      values: [{
+        date: '01.01.2018',
+        value: 18000
+      }, {
+        date: '02.01.2019',
+        value: 22000
+      }]
+    },
+    {
+      id: 4587,
+      name: 'Do excepteur est culpa et commodo duis.',
+      values: [{
+        date: '31.12.2018',
+        value: 5000
+      }, {
+        date: '31.01.2019',
+        value: 6000
+      }]
+    },
+    {
+      id: 9512,
+      name: 'Do excepteur est culpa et commodo duis.',
+      values: [{
+        date: '20.01.2018',
+        value: 95230
+      }, {
+        date: '26.01.2019',
+        value: 95743
+      }, {
+        date: '02.02.2019',
+        value: 35741
+      }]
+    },
+    {
+      id: 95147,
+      name: 'Do excepteur est culpa et commodo duis.',
+      values: [{
+        date: '03.02.2018',
+        value: 85413
+      }, {
+        date: '15.02.2019',
+        value: 65893
+      }]
+    }
+  ]
+}
