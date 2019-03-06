fd7d95fe2422bb06f46b07a51fe9e554b3dc23ea
Anatoliy Pechkin <anatmozg@mail.ru>
2019-03-06 16:19:32 +0300

    emit drag events

--- src/components/RDraggableArea/index.vue
+++ src/components/RDraggableArea/index.vue
@@ -1,14 +1,25 @@
 <template>
-  <div
-    class="drag-component"
-    :class="computedStyle"
-    :style="styles"
-    :draggable="draggable"
-    @mouseenter="onEnter"
-    @mouseleave="onLeave"
-    @click="onClick"
-  >
-    <slot :entered="computeEntered"/>
+  <div class="drag-wrapperr">
+    <div class="drag-fakeelement prevent" />
+    <div
+      ref="dragComponent"
+      class="drag-component"
+      :draggable="draggable"
+      :class="computedStyle"
+      :style="styles"
+      @mouseenter="onEnter"
+      @mouseleave="onLeave"
+      @click="onClick"
+      @dragstart="event => $emit('dragstart', event)"
+      @drag="event => $emit('drag', event)"
+      @dragend="event => $emit('dragend', event)"
+      @dragover="event => $emit('dragover', event)"
+      @dragenter="event => $emit('dragenter', event)"
+      @dragleave="event => $emit('dragleave', event)"
+      @drop="event => $emit('drop', event)"
+    >
+      <slot :entered="computeEntered"/>
+    </div>
   </div>
 </template>
 
@@ -31,7 +42,8 @@
     data () {
       return {
         entered: false,
-        selected: false
+        selected: false,
+        enterTarget: null
       }
     },
     methods: {
@@ -64,6 +76,12 @@
 <style lang="scss" scoped>
   @import "../../assets/mixins.scss";
   @import "../../assets/style.scss";
+  .drag-wrapperr {
+    display: inline-block;
+    position: relative;
+    min-width: $tbl_header_width;
+    min-height: $tbl_ce_height;
+  }
   .drag-component {
     display: inline-block;
     position: relative;
@@ -73,6 +91,7 @@
     text-align: right;
     cursor: pointer;
     border: 2px solid white;
+    z-index: 10;
   }
   .selected {
     border: 2px solid $orange_5!important;
@@ -82,5 +101,13 @@
   }
   .edited {
     border: 2px solid $orange_5;
+  }
+  .drag-fakeelement {
+    position: absolute;
+    width: $tbl_header_width;
+    height: $tbl_ce_height;
+    z-index: 9;
+    background-color: $gray_3;
+    pointer-events: none;
   }
 </style>
