c19b4287ae03c9e67d699899fa86155ef704552e
Anatoliy Pechkin <anatmozg@mail.ru>
2019-03-06 16:22:42 +0300

    add drag events handlers

--- src/containers/EDragInput/index.vue
+++ src/containers/EDragInput/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <rdrag-block :editMode="editMode">
+  <rdrag-block
+    :editMode="editMode"
+    :draggable="draggable"
+    @dragstart="dragStart"
+    @drag="dragTarget"
+    @dragend="dragEnd"
+    @dragover="dragOver"
+    @dragenter="dragEnter"
+    @dragleave="dragLeave"
+    @drop="dropToTarget"
+  >
     <template v-slot="{ entered }">
       <r-input
         :editMode="editMode"
@@ -37,7 +47,11 @@
       currency: [String],
       curnum: [Number, String],
       value: [Number, String],
-      type: String
+      type: String,
+      draggable: {
+        type: Boolean,
+        default: true
+      }
     },
     data () {
       return {
@@ -54,7 +68,11 @@
           4: '£'
         },
         editMode: false,
-        componentValue: this.value
+        componentValue: this.value,
+        dragTargetElement: {
+          target: null,
+          type: null
+        }
       }
     },
     methods: {
@@ -78,6 +96,55 @@
       },
       handleChangeValue (val) {
         this.componentValue = val
+      },
+      dragStart (event) {
+        console.log('drag start')
+        if (event.fromElement === null || typeof event.fromElement === 'undefined') {
+          this.dragTargetElement['target'] = JSON.stringify(event.target).replace(/ entered/, '')
+          this.dragTargetElement['type'] = event.type
+        }
+        console.log(this.dragTargetElement)
+      },
+      dragTarget (event) {
+        event.target.style.zIndex = 8
+      },
+      dragEnd (event) {
+        event.target.style.zIndex = 10
+        event.target.style.cursor = 'pointer'
+      },
+      dragEnter (event) {
+        // event.preventDefault() // обязательно
+        // console.log('dragEnter on:', id)
+        console.log('dragEnter:', JSON.stringify(event.target))
+        console.log('dragEnter type:', typeof JSON.stringify(event.target))
+        event.stopPropagation()
+        event.preventDefault()
+        if (JSON.stringify(event.target).replace(/ entered/, '') == this.dragTargetElement['target']) {
+          console.log('enter to another element')
+
+          // event.target.style.border = '2px solid #339af0'
+        } else {
+          console.log(JSON.stringify(event.target).replace(/ entered/, ''), ' not equal: ', this.dragTargetElement['target'])
+        }
+      },
+      dragOver (event) {
+        // console.log('dragOver')
+        // console.log(event)
+        event.stopPropagation()
+        event.preventDefault()
+      },
+      dragLeave (event) {
+        // console.log('dragLeave id:', id)
+        console.log('dragLeave:', event)
+        if (event.target.className === 'drag-component') {
+          event.target.style.border = ''
+        }
+      },
+      dragExit (event) {
+        console.log('dragExit')
+      },
+      dropToTarget (event) {
+        console.log('drop r-fixed')
       }
     },
     computed: {
