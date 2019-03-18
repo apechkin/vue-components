6c32f802e239bc48cf86521575bb0bf296b9051f
Anatoliy Pechkin <anatmozg@mail.ru>
2019-03-18 12:59:14 +0300

    add some fake income from client data

--- src/pages/Content/fakeData.js
+++ src/pages/Content/fakeData.js
@@ -7,6 +7,30 @@ export const clientData = () => {
 }
 const fromClient = [
   {
+    fullDate: '2018-12-25',
+    fromClient: 800000
+  },
+  {
+    fullDate: '2018-12-28',
+    fromClient: 350000
+  },
+  {
+    fullDate: '2019-01-09',
+    fromClient: 1000000
+  },
+  {
+    fullDate: '2019-01-12',
+    fromClient: 80000000
+  },
+  {
+    fullDate: '2019-01-25',
+    fromClient: 82000000
+  },
+  {
+    fullDate: '2019-01-29',
+    fromClient: 52654123
+  },
+  {
     fullDate: '2018-12-31',
     fromClient: 150145952
   },
