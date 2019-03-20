a93ae3586a46bb6455891d6c01f3ca787ae7ea10
Anatoliy Pechkin <anatmozg@mail.ru>
2019-03-20 18:01:11 +0300

    cahsflow module

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import dragTable from './modules/dragTable.js'
-import calendar from './modules/calendar.js'
+import cashFlow from './modules/cashFlow.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     dragTable,
-    calendar
+    cashFlow
   }
 })
